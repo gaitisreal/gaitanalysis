d27e21d20e85ea67f8d8c33e39e7af91a49690ef
aereneth <zerify@gmail.com>
2016-11-01 10:25:08 +0800

    Detect Distance
    Distance using Pythagorean Theorem

--- Kinect/Kinect/Program.cs
+++ Kinect/Kinect/Program.cs
@@ -46,6 +46,8 @@ namespace Kinect
 
         private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
+            int ctr = 0;
+            double cm = 0;
             // Access the skeleton frame
             using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
             {
@@ -76,128 +78,19 @@ namespace Kinect
                         Joint hipRight = skeleton.Joints[JointType.HipRight];
                         Joint hipCenter = skeleton.Joints[JointType.HipCenter];
 
-                        if (kneeLeft.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Left Knee: " + kneeLeft.Position.X + ", " + kneeLeft.Position.Y + ", " + kneeLeft.Position.Z);
-                        if (kneeRight.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Right Knee: " + kneeRight.Position.X + ", " + kneeRight.Position.Y + ", " + kneeRight.Position.Z);
-                        Console.WriteLine();
-                        if (ankleLeft.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Left Ankle: " + ankleLeft.Position.X + ", " + ankleLeft.Position.Y + ", " + ankleLeft.Position.Z);
-                        if (ankleRight.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Right Ankle: " + ankleRight.Position.X + ", " + ankleRight.Position.Y + ", " + ankleRight.Position.Z);
-                        Console.WriteLine();
-                        if (footLeft.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Left Foot: " + footLeft.Position.X + ", " + footLeft.Position.Y + ", " + footLeft.Position.Z);
-                        if (footRight.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Right Foot: " + footRight.Position.X + ", " + footRight.Position.Y + ", " + footRight.Position.Z);
-                        Console.WriteLine();
-                        if (hipLeft.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Left Hip: " + hipLeft.Position.X + ", " + hipLeft.Position.Y + ", " + hipLeft.Position.Z);
-                        if (hipRight.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Right Hip: " + hipRight.Position.X + ", " + hipRight.Position.Y + ", " + hipRight.Position.Z);
-                        if (hipCenter.TrackingState == JointTrackingState.Tracked)
-                            Console.WriteLine("Center Hip: " + hipCenter.Position.X + ", " + hipCenter.Position.Y + ", " + hipCenter.Position.Z);
+                        //Console.WriteLine(ankleLeft.Position.X + " " + ankleLeft.Position.Y + " " + ankleLeft.Position.Z);
+
+                        double distance = Math.Sqrt(Math.Pow(skeleton.Joints[JointType.HandRight].Position.X - skeleton.Joints[JointType.HandLeft].Position.X, 2) + Math.Pow(skeleton.Joints[JointType.HandRight].Position.Y - skeleton.Joints[JointType.HandLeft].Position.Y, 2) +
+                            Math.Pow(skeleton.Joints[JointType.HandRight].Position.Z - skeleton.Joints[JointType.HandLeft].Position.Z, 2));
+                        double stepLength = Math.Round(distance * 100, 2);
+
+                        /*if ((ankleLeft.Position.Y > -0.54 && ankleLeft.Position.Y < -0.53 && ankleRight.Position.Y > -0.54 && ankleRight.Position.Y < -0.53) &&
+                            (ankleLeft.Position.Y > 1.79 && ankleLeft.Position.Y < 1.80 && ankleRight.Position.Y > 1.79 && ankleRight.Position.Y < 1.80))*/
+                        //Console.Clear();
+                        Console.WriteLine(stepLength);
                     }
                 }
             }
         }
     }
-    /*class Program
-    {
-        KinectSensor kinect = null;
-        Skeleton[] skeletonData = null;
-
-        static void Main(string[] args)
-        {
-            Program program = new Program();
-            program.StartKinectST();
-            Console.ReadLine();
-        }
-
-        void StartKinectST()
-        {
-            kinect = KinectSensor.KinectSensors.FirstOrDefault(s => s.Status == KinectStatus.Connected); // Get first Kinect Sensor
-            kinect.SkeletonStream.Enable(); // Enable skeletal tracking
-
-            skeletonData = new Skeleton[kinect.SkeletonStream.FrameSkeletonArrayLength]; // Allocate ST data
-
-            kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinect_SkeletonFrameReady); // Get Ready for Skeleton Ready Events
-
-            kinect.Start(); // Start Kinect sensor
-        }
-
-        private void kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
-        {
-            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame()) // Open the Skeleton frame
-            {
-                if (skeletonFrame != null && this.skeletonData != null) // check that a frame is available
-                {
-                    skeletonFrame.CopySkeletonDataTo(this.skeletonData); // get the skeletal information in this frame
-                }
-
-                foreach (var skeleton in skeletonData)
-                {
-                    // skip the skeleton if it is not being tracked
-                    if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
-                        continue;
-
-                    // print the RightHand Joint position to the debug console
-                    Console.WriteLine(skeleton.Joints[JointType.HandRight]);
-                }
-            }
-        }
-
-        private void DrawSkeletons()
-        {
-            foreach (Skeleton skeleton in this.skeletonData)
-            {
-                if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
-                {
-                    DrawTrackedSkeletonJoints(skeleton.Joints);
-                }
-                else if (skeleton.TrackingState == SkeletonTrackingState.PositionOnly)
-                {
-                    //DrawSkeletonPosition(skeleton.Position);
-                }
-            }
-        }
-
-        private void DrawTrackedSkeletonJoints(JointCollection jointCollection)
-        {
-            // Render Head and Shoulders
-            DrawBone(jointCollection[JointType.Head], jointCollection[JointType.ShoulderCenter]);
-            DrawBone(jointCollection[JointType.ShoulderCenter], jointCollection[JointType.ShoulderLeft]);
-            DrawBone(jointCollection[JointType.ShoulderCenter], jointCollection[JointType.ShoulderRight]);
-
-            // Render Left Arm
-            DrawBone(jointCollection[JointType.ShoulderLeft], jointCollection[JointType.ElbowLeft]);
-            DrawBone(jointCollection[JointType.ElbowLeft], jointCollection[JointType.WristLeft]);
-            DrawBone(jointCollection[JointType.WristLeft], jointCollection[JointType.HandLeft]);
-
-            // Render Right Arm
-            DrawBone(jointCollection[JointType.ShoulderRight], jointCollection[JointType.ElbowRight]);
-            DrawBone(jointCollection[JointType.ElbowRight], jointCollection[JointType.WristRight]);
-            DrawBone(jointCollection[JointType.WristRight], jointCollection[JointType.HandRight]);
-
-            // Render other bones...
-        }
-
-        private void DrawBone(Joint jointFrom, Joint jointTo)
-        {
-            if (jointFrom.TrackingState == JointTrackingState.NotTracked || jointTo.TrackingState == JointTrackingState.NotTracked)
-            {
-                return; // nothing to draw, one of the joints is not tracked
-            }
-
-            if (jointFrom.TrackingState == JointTrackingState.Inferred || jointTo.TrackingState == JointTrackingState.Inferred)
-            {
-                //DrawNonTrackedBoneLine(jointFrom.Position, jointTo.Position);  // Draw thin lines if either one of the joints is inferred
-            }
-
-            if (jointFrom.TrackingState == JointTrackingState.Tracked && jointTo.TrackingState == JointTrackingState.Tracked)
-            {
-                //DrawTrackedBoneLine(jointFrom.Position, jointTo.Position);  // Draw bold lines if the joints are both tracked
-            }
-        }
-    }*/
 }
