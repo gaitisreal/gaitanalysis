8727d8cd449501b5cbe5db00657ddb4b3e5c079e
aereneth <zerify@gmail.com>
2016-11-01 12:56:13 +0800

    Revert "Parameters"
    This reverts commit e398709971e64fee54022e239dcdc4758151b81c.

--- Kinect/Kinect/Program.cs
+++ Kinect/Kinect/Program.cs
@@ -35,25 +35,6 @@ namespace Kinect
 
     internal class Tracker
     {
-        int frame = 0;
-        DateTime time = DateTime.Now;
-        //double minutes = time.Minute;
-
-        //Gait Parameters
-        double cadence = 0;
-        double stepLength = 0;
-        double stepTime = 0;
-        double stepWidth = 0;
-        double stanceTime = 0;
-        double strideLength = 0;
-        double strideVelocity = 0;
-        double swingTime = 0;
-
-        double totalDistance = 0;
-
-        double[] initialPoint = new double[3];
-        double[] currentPoint = new double[3];
-
         private Skeleton[] skeletons = null;
 
         public Tracker(KinectSensor sensor)
@@ -61,13 +42,12 @@ namespace Kinect
             // Connect the skeleton frame handler and enable skeleton tracking
             sensor.SkeletonFrameReady += SensorSkeletonFrameReady;
             sensor.SkeletonStream.Enable();
-            sensor.DepthStream.Enable();
         }
-
-        public object FloorClipPane { get; private set; }
 
         private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
+            int ctr = 0;
+            double cm = 0;
             // Access the skeleton frame
             using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
             {
@@ -87,51 +67,27 @@ namespace Kinect
 
                     if (skeleton != null)
                     {
-                        // Obtain parameters; if tracked, print its position
+                        // Obtain the left knee joint; if tracked, print its position
                         Joint kneeLeft = skeleton.Joints[JointType.KneeLeft];
                         Joint kneeRight = skeleton.Joints[JointType.KneeRight];
                         Joint ankleLeft = skeleton.Joints[JointType.AnkleLeft];
                         Joint ankleRight = skeleton.Joints[JointType.AnkleRight];
                         Joint footLeft = skeleton.Joints[JointType.FootLeft];
                         Joint footRight = skeleton.Joints[JointType.FootRight];
-                        //Joint hipLeft = skeleton.Joints[JointType.HipLeft];
-                        //Joint hipRight = skeleton.Joints[JointType.HipRight];
-                        //Joint hipCenter = skeleton.Joints[JointType.HipCenter];
+                        Joint hipLeft = skeleton.Joints[JointType.HipLeft];
+                        Joint hipRight = skeleton.Joints[JointType.HipRight];
+                        Joint hipCenter = skeleton.Joints[JointType.HipCenter];
 
-                        //Step Length
-                        if (ankleLeft.TrackingState == JointTrackingState.Tracked || ankleLeft.TrackingState == JointTrackingState.Tracked)
-                        {
-                            stepLength = Math.Round(Math.Sqrt(Math.Pow(ankleRight.Position.X - ankleLeft.Position.X, 2) +
-                                        Math.Pow(ankleRight.Position.Y - ankleLeft.Position.Y, 2) +
-                                        Math.Pow(ankleRight.Position.Z - ankleLeft.Position.Z, 2)) * 100, 2);
-                        }
+                        //Console.WriteLine(ankleLeft.Position.X + " " + ankleLeft.Position.Y + " " + ankleLeft.Position.Z);
 
-                        //Distance Traveled
-                        if (footLeft.TrackingState == JointTrackingState.Tracked)
-                        {
-                            if(initialPoint[0] == 0 && initialPoint[1] == 0 && initialPoint[2] == 0)
-                            {
-                                initialPoint[0] = footLeft.Position.X;
-                                initialPoint[1] = footLeft.Position.Y;
-                                initialPoint[2] = footLeft.Position.Z;
-                            }
-                            else
-                            {
-                                currentPoint[0] = footLeft.Position.X;
-                                currentPoint[1] = footLeft.Position.Y;
-                                currentPoint[2] = footLeft.Position.Z;
-                                totalDistance = Math.Round(Math.Sqrt(Math.Pow(initialPoint[0] - currentPoint[0], 2) +
-                                                Math.Pow(initialPoint[0] - currentPoint[0], 2) +
-                                                Math.Pow(initialPoint[0] - currentPoint[0], 2)) * 100, 2);
-                            }
-                        }
+                        double distance = Math.Sqrt(Math.Pow(skeleton.Joints[JointType.HandRight].Position.X - skeleton.Joints[JointType.HandLeft].Position.X, 2) + Math.Pow(skeleton.Joints[JointType.HandRight].Position.Y - skeleton.Joints[JointType.HandLeft].Position.Y, 2) +
+                            Math.Pow(skeleton.Joints[JointType.HandRight].Position.Z - skeleton.Joints[JointType.HandLeft].Position.Z, 2));
+                        double stepLength = Math.Round(distance * 100, 2);
 
-                        Console.Clear();
-                        Console.WriteLine("Frame: " + ++frame);
-                        Console.WriteLine("Step Length: " + stepLength + "cm");
-                        Console.WriteLine("Distance Traveled: " + totalDistance + "cm");
-                        Console.WriteLine(time.Hour + ":" + time.Minute + ":" + time.Second);
-                        Console.WriteLine();
+                        /*if ((ankleLeft.Position.Y > -0.54 && ankleLeft.Position.Y < -0.53 && ankleRight.Position.Y > -0.54 && ankleRight.Position.Y < -0.53) &&
+                            (ankleLeft.Position.Y > 1.79 && ankleLeft.Position.Y < 1.80 && ankleRight.Position.Y > 1.79 && ankleRight.Position.Y < 1.80))*/
+                        //Console.Clear();
+                        Console.WriteLine(stepLength);
                     }
                 }
             }
